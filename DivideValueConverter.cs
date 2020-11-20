d58767d42bc26a14f05cd4403140f16fe0a20dd1
zqm980904 <zqm980904@gmail.com>
2020-11-20 20:36:01 +0800

    引入dragablz

---
+++ DivideValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+// Cited from https://github.com/Keboo/MaterialDesignInXaml.Examples/blob/master/Dragablz/TabablzControl.FullWidthTabs/DivideValueConverter.cs
+
+namespace TimeManagement
+{
+    public class DivideValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) / System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
