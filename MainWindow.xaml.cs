8a699f84551a69d1d8b9021d32b34d94ff4dd187
Zhao Qiming <zqm980904@gmail.com>
2020-11-28 15:06:05 +0800

    分割主窗口，实现暗色主题调整

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,45 @@ namespace TimeManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Snackbar Snackbar = new Snackbar();
         public MainWindow()
         {
             InitializeComponent();
+
+            Task.Factory.StartNew(() => Thread.Sleep(2500)).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue?.Enqueue("Welcome to TimeManagement");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue!);
+
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            DarkModeToggleButton.IsChecked = theme.GetBaseTheme() == BaseTheme.Dark;
+
+            if (paletteHelper.GetThemeManager() is { } themeManager)
+            {
+                themeManager.ThemeChanged += (_, e)
+                    => DarkModeToggleButton.IsChecked = e.NewTheme?.GetBaseTheme() == BaseTheme.Dark;
+            }
+
+            Snackbar = MainSnackbar;
+
+        }
+
+        private void MenuDarkModeButton_Click(object sender, RoutedEventArgs e)
+            => ModifyTheme(DarkModeToggleButton.IsChecked == true);
+
+        private static void ModifyTheme(bool isDarkTheme)
+        {
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+            paletteHelper.SetTheme(theme);
         }
     }
 }
