634ad997a4f63d91889e57b313f92f6fd1e79f5b
Zhao Qiming <zqm980904@gmail.com>
2020-11-29 15:54:15 +0800

    更新窗口样式绑定

---
+++ Themes/MaterialDesignFontSC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace TimeManagement.Themes
+{
+    [MarkupExtensionReturnType(typeof(FontFamily))]
+    public class MaterialDesignFontSCExtension : MarkupExtension
+    {
+        private static readonly Lazy<FontFamily> _notosanssc
+            = new Lazy<FontFamily>(() =>
+                new FontFamily(new Uri("pack://application:,,,/Resources/NotoSansSC/"), "./#Noto Sans SC"));
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _notosanssc.Value;
+        }
+    }
+}
