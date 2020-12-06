6cc878587521526479565f8f94e4ff043c13fca3
Viridian <zy-zhao17@mails.tsinghua.edu.cn>
2020-12-06 10:27:55 +0800

    终于解决了verticalalignment不能stretch的问题！！！

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,48 +45,50 @@ namespace TimeManagement
         protected override void OnActivated(EventArgs e)
         {
             base.OnActivated(e);
+            //return;
 
             //////////////////////////////////////////////////垂直alignment设置成Stretch为什么不管用！！！！！！！！！！！！！！！！！！！！！！
             planGrid.Children.Clear();
             planGrid.RowDefinitions.Clear();
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < 10; i++)
             {
                 
-                planGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100+i, GridUnitType.Star) });
+                planGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(random.NextDouble(), GridUnitType.Star) });
 
 
                 Button b = new Button();
                 planGrid.Children.Add(b);
                 b.SetValue(Grid.RowProperty, i);
-                b.Margin = new Thickness(5, 2, 5, 2);
+                b.Margin = new Thickness(5, 2, 3, 2);
                 //b.VerticalAlignment = VerticalAlignment.Stretch;
+                
 
                 SolidColorBrush c = new SolidColorBrush(Color.FromArgb(255, (byte)(random.NextDouble() * 256), (byte)(random.NextDouble() * 256), 255));
                 b.Background = c;
                 b.BorderBrush = c;
-                b.Content = "plan";
-
+                b.Content = "plan" + i.ToString();
+                b.Height = double.NaN;
             }
 
             actualGrid.Children.Clear();
             actualGrid.RowDefinitions.Clear();
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < 10; i++)
             {
 
-                actualGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100+15-i, GridUnitType.Star) });
+                actualGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(random.NextDouble(), GridUnitType.Star) });
 
 
                 Button b = new Button();
                 actualGrid.Children.Add(b);
                 b.SetValue(Grid.RowProperty, i);
-                b.Margin = new Thickness(5, 2, 5, 2);
+                b.Margin = new Thickness(3, 2, 5, 2);
                 //b.VerticalAlignment = VerticalAlignment.Stretch;
 
                 SolidColorBrush c = new SolidColorBrush(Color.FromArgb(255, (byte)(random.NextDouble() * 256), (byte)(random.NextDouble() * 256), 255));
                 b.Background = c;
                 b.BorderBrush = c;
-                b.Content = "actual";
-
+                b.Content = "actual" + i.ToString();
+                b.Height = double.NaN;
             }
 
         }
