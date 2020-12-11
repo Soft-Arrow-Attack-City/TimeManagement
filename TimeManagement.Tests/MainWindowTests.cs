48c8dfb4078de06e0c4c676666a88fd529f290b4
Zhao Qiming <zqm980904@gmail.com>
2020-12-11 20:41:30 +0800

    添加UI测试

---
+++ TimeManagement.Tests/MainWindowTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TimeManagement.Tests
+{
+    public class MainWindowTests
+    {
+        [StaFact]
+        public void MainWindowTitle()
+        {
+            MainWindow mainWindow = new MainWindow();
+            Assert.Equal("MainWindow", mainWindow.Title);
+        }
+    }
+}
