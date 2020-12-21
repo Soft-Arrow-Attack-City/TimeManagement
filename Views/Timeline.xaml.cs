85877dfd80321b37090b31e0447b17253f9d0c5e
Viridian <zy-zhao17@mails.tsinghua.edu.cn>
2020-12-21 17:55:37 +0800

    时间线实际使用部分前端逻辑完成

--- Views/Timeline.xaml.cs
+++ Views/Timeline.xaml.cs
@@ -48,15 +48,22 @@ namespace TimeManagement.Views
             drawTimeline();
         }
 
-        //将真的或者假的数据载入进来，并进行一定的处理
-        private void LoadData()
+        //这个消息响应让时间跟着窗口大小变化。
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
+            base.OnRenderSizeChanged(sizeInfo);
+            drawTime();
+        }
 
-            ScreenUsage1h = new string[24];
-            ScreenUsage30min = new string[48];
-            ScreenUsage15min = new string[96];
-            ScreenUsage5min = new string[288];
-            ScreenUsage1min = new string[1440];
+        //将真的或者假的数据载入进来，并进行一定的处理
+        private bool LoadData()
+        {
+            //修复24:00:00的bug，要加1。
+            ScreenUsage1h = new string[24 + 1];
+            ScreenUsage30min = new string[48 + 1];
+            ScreenUsage15min = new string[96 + 1];
+            ScreenUsage5min = new string[288 + 1];
+            ScreenUsage1min = new string[1440 + 1];
 
             //获取真的或者假的屏幕使用记录数据，要排好序的。尽管其中的时间粒度可能很粗或者很细，很不均匀。
             List<DataModel.Alog> winlog = DataModel.TimelineData.generatedata();
@@ -69,6 +76,8 @@ namespace TimeManagement.Views
 
 
             int nlogs = winlog.Count;
+            if (nlogs == 0) return false;//当日没有数据！
+
             int now1min = 0;
             int now5min = 0;
             int now15min = 0;
@@ -84,12 +93,14 @@ namespace TimeManagement.Views
                     }
                     else count1min.Add(winlog[i].s, 1);
                 }
-                else if (count1min.Count > 0)
+                else
                 {
-
-                    ScreenUsage1min[now1min]=count1min.Where(kvp => kvp.Value == count1min.Max(kvp => kvp.Value)).First().Key;
-                    now1min = winlog[i].t / 60;
-                    count1min.Clear();
+                    if (count1min.Count > 0)
+                    {
+                        ScreenUsage1min[now1min] = count1min.Where(kvp => kvp.Value == count1min.Max(kvp => kvp.Value)).First().Key;
+                        count1min.Clear();
+                    }
+                    now1min = winlog[i].t / 60; 
                     count1min.Add(winlog[i].s, 1);
                 }
 
@@ -101,11 +112,15 @@ namespace TimeManagement.Views
                     }
                     else count5min.Add(winlog[i].s, 1);
                 }
-                else if (count5min.Count > 0)
+                else 
                 {
-                    ScreenUsage5min[now5min] = count5min.Where(kvp => kvp.Value == count5min.Max(kvp => kvp.Value)).First().Key;
+                    if (count5min.Count > 0)
+                    {
+                        ScreenUsage5min[now5min] = count5min.Where(kvp => kvp.Value == count5min.Max(kvp => kvp.Value)).First().Key;
+                        count5min.Clear();
+                    }
+                        
                     now5min = winlog[i].t / 300;
-                    count5min.Clear();
                     count5min.Add(winlog[i].s, 1);
                 }
 
@@ -117,11 +132,15 @@ namespace TimeManagement.Views
                     }
                     else count15min.Add(winlog[i].s, 1);
                 }
-                else if (count15min.Count > 0)
+                else 
                 {
-                    ScreenUsage15min[now15min] = count15min.Where(kvp => kvp.Value == count15min.Max(kvp => kvp.Value)).First().Key;
+                    if (count15min.Count > 0)
+                    {
+                        ScreenUsage15min[now15min] = count15min.Where(kvp => kvp.Value == count15min.Max(kvp => kvp.Value)).First().Key;
+                        count15min.Clear();
+                    }
+                        
                     now15min = winlog[i].t / 900;
-                    count15min.Clear();
                     count15min.Add(winlog[i].s, 1);
                 }
 
@@ -133,11 +152,15 @@ namespace TimeManagement.Views
                     }
                     else count30min.Add(winlog[i].s, 1);
                 }
-                else if (count30min.Count > 0)
+                else 
                 {
-                    ScreenUsage30min[now30min] = count30min.Where(kvp => kvp.Value == count30min.Max(kvp => kvp.Value)).First().Key;
+                    if (count30min.Count > 0)
+                    {
+                        ScreenUsage30min[now30min] = count30min.Where(kvp => kvp.Value == count30min.Max(kvp => kvp.Value)).First().Key;
+                        count30min.Clear();
+                    }
+                        
                     now30min = winlog[i].t / 1800;
-                    count30min.Clear();
                     count30min.Add(winlog[i].s, 1);
                 }
 
@@ -148,22 +171,24 @@ namespace TimeManagement.Views
                         count1h[winlog[i].s] += 1;
                     }
                     else count1h.Add(winlog[i].s, 1);
-                }///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////有bug：没有处理最后一个块的内容；clear和add不应只在count>0时发生。
-                else if (count1h.Count > 0)
+                }
+                else
                 {
-                    ScreenUsage1h[now1h] = count1h.Where(kvp => kvp.Value == count1h.Max(kvp => kvp.Value)).First().Key;
-                    count1h.Clear();
+                    if (count1h.Count > 0)
+                    {
+                        ScreenUsage1h[now1h] = count1h.Where(kvp => kvp.Value == count1h.Max(kvp => kvp.Value)).First().Key;
+                        count1h.Clear();
+                    }
+                    now1h = winlog[i].t / 3600;
                     count1h.Add(winlog[i].s, 1);
                 }
-
-
-                now1min = winlog[i].t / 60;
-                now5min = winlog[i].t / 300;
-                now15min = winlog[i].t / 900;
-                now30min = winlog[i].t / 1800; 
-                now1h = winlog[i].t / 3600;
             }
-
+            ScreenUsage1min[now1min] = count1min.Where(kvp => kvp.Value == count1min.Max(kvp => kvp.Value)).First().Key;
+            ScreenUsage5min[now5min] = count5min.Where(kvp => kvp.Value == count5min.Max(kvp => kvp.Value)).First().Key;
+            ScreenUsage15min[now15min] = count15min.Where(kvp => kvp.Value == count15min.Max(kvp => kvp.Value)).First().Key;
+            ScreenUsage30min[now30min] = count30min.Where(kvp => kvp.Value == count30min.Max(kvp => kvp.Value)).First().Key;
+            ScreenUsage1h[now1h] = count1h.Where(kvp => kvp.Value == count1h.Max(kvp => kvp.Value)).First().Key;
+            return true;
         }
 
 
@@ -175,6 +200,7 @@ namespace TimeManagement.Views
             //绘制计划的时间线，暂时先用随机数据。
             planGrid.Children.Clear();
             planGrid.RowDefinitions.Clear();
+            /*
             for (int i = 0; i < 10; i++)
             {
                 planGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(random.NextDouble(), GridUnitType.Star) });
@@ -189,29 +215,80 @@ namespace TimeManagement.Views
                 b.Content = "plan" + i.ToString();
 
             }
-
+            */
             //绘制实际的时间线。采用winlog中的数据。
-
-
-
-
             actualGrid.Children.Clear();
             actualGrid.RowDefinitions.Clear();
             //只能显示出从startsecond到endsecond的部分，而且只能显示某一天。（按说应该是会给一个选择日期的按钮的）
+            //小时模式：时间跨度大于8小时。
+            //一屏放14个左右的块是舒服的
+            string[] usingstring = ScreenUsage1h;
+            int interval = 3600;
 
-            
-            for (int i = 0; i < 24; i++)
+            if (endsecond - startsecond < 1800*14)
             {
-                actualGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                usingstring = ScreenUsage30min;
+                interval = 1800;
+            }
+            if (endsecond - startsecond < 900*14)
+            {
+                usingstring = ScreenUsage15min;
+                interval = 900;
+            }
+            if (endsecond - startsecond < 300*14)
+            {
+                usingstring = ScreenUsage5min;
+                interval = 300;
+            }
+            if (endsecond - startsecond < 60*14)
+            {
+                usingstring = ScreenUsage1min;
+                interval = 60;
+            }
+
+
+            int realcount = 0;
+            for (int i = (int)(startsecond/ interval); i <=(int)(endsecond/ interval);i++)
+            {
+                if ((endsecond / interval) == i) break;//这一行可以解决24:00导致数组越界的bug！
+
+                double height = interval;
+                if (i == (int)(startsecond / interval)) height += (i * interval - startsecond);
+                if (i == (int)(endsecond / interval)) height -= (i * interval - endsecond + interval);
+                
+                string nowstring = usingstring[i];
+
+
+                
+                while ((usingstring[i + 1] == nowstring) && (i < (int)(endsecond / interval)))
+                {
+                    i++;
+                    height += interval;
+                    if (i == (int)(startsecond / interval)) height += (i * interval - startsecond);
+                    if (i == (int)(endsecond / interval)) height -= (i * interval - endsecond + interval);
+
+                }
+
+                
+
+                //如果和下面的相同，就一起做了！
+                actualGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(height, GridUnitType.Star) });
                 Button b = new Button();
                 actualGrid.Children.Add(b);
-                b.SetValue(Grid.RowProperty, i);
+                b.SetValue(Grid.RowProperty,realcount++);
                 b.Margin = new Thickness(3, 2, 5, 2);
                 b.Height = double.NaN;
-                SolidColorBrush c = new SolidColorBrush(Color.FromArgb(200, (byte)(random.NextDouble() * 256), (byte)(random.NextDouble() * 256), 255));
+                if (nowstring == null)
+                {
+                    b.Opacity = 0;
+                    continue;
+                }
+                b.Content = nowstring;
+                SolidColorBrush c = new SolidColorBrush(Color.FromArgb(200, (byte)nowstring.GetHashCode(), (byte)(nowstring.GetHashCode()/256), 255));
                 b.Background = c;
                 b.BorderBrush = c;
-                b.Content = ScreenUsage1h[i];
+                
+
 
             }
             
@@ -221,95 +298,57 @@ namespace TimeManagement.Views
         public void drawTime()
         {
             timeGrid.Children.Clear();
-            //4小时刻度出现：0，4，8，12，16，20，24。（一直都在）
-            for (int drawtime = (int)startsecond / 60 / 60 / 4; drawtime <= endsecond / 60 / 60 / 4; drawtime++)
+
+            int interval = 60 * 60 * 4;
+            int mod = 1;
+            int div = 1;
+            int multi = 0;
+            if (endsecond - startsecond < 12 * 60 * 60)
+            {
+                interval = 60*60;
+                mod = 1;
+                div = 4;
+                multi = 0;
+            }
+            if (endsecond - startsecond < 3 * 60 * 60)
+            {
+                interval = 60 * 15;
+                mod = 4;
+                div = 16;
+                multi = 15;
+            }
+            if (endsecond - startsecond < 45 * 60)
+            {
+                interval = 60 * 5;
+                mod = 12;
+                div = 48;
+                multi = 5;
+            }
+            if (endsecond - startsecond < 10 * 60)
+            {
+                interval = 60;
+                mod = 60;
+                div = 240;
+                multi = 1;
+            }
+
+
+            for (int drawtime = (int)startsecond / interval; drawtime <= endsecond / interval; drawtime++)
             {
 
-                double margin = timeGrid.ActualHeight * (drawtime * 60 * 60 * 4 - startsecond) / (endsecond - startsecond) - 20;
+                double margin = timeGrid.ActualHeight * (drawtime * interval - startsecond) / (endsecond - startsecond) - 20;
                 if (margin > -30)
                 {
                     TextBlock a = new TextBlock();
-                    a.Text = (drawtime * 4).ToString("D2") + ":00";
+                    a.Text = (drawtime * 4 / div).ToString("D2") + ":" + (drawtime % mod * multi).ToString("D2");
                     a.HorizontalAlignment = HorizontalAlignment.Right;
                     a.Margin = new Thickness(0, margin, 0, 0);
                     timeGrid.Children.Add(a);
-                }
-
-            }
-            //1小时刻度出现：当间隔小于12小时
-            if (endsecond - startsecond < 12 * 60 * 60)
-            {
-                for (int drawtime = (int)startsecond / 60 / 60; drawtime <= endsecond / 60 / 60; drawtime++)
-                {
-                    double margin = timeGrid.ActualHeight * (drawtime * 60 * 60 - startsecond) / (endsecond - startsecond) - 20;
-                    if (margin > -30)
-                    {
-                        TextBlock a = new TextBlock();
-                        a.Text = drawtime.ToString("D2") + ":00";
-                        a.HorizontalAlignment = HorizontalAlignment.Right;
-                        a.Margin = new Thickness(0, margin, 0, 0);
-                        timeGrid.Children.Add(a);
-                    }
 
                 }
             }
 
-            //15分钟刻度出现：当间隔小于3小时
-            if (endsecond - startsecond < 3 * 60 * 60)
-            {
-                for (int drawtime = (int)startsecond / 60 / 15; drawtime <= endsecond / 60 / 15; drawtime++)
-                {
 
-                    double margin = timeGrid.ActualHeight * (drawtime * 60 * 15 - startsecond) / (endsecond - startsecond) - 20;
-                    if (margin > -30)
-                    {
-                        TextBlock a = new TextBlock();
-                        a.Text = (drawtime / 4).ToString("D2") + ":" + (drawtime % 4 * 15).ToString("D2");
-                        a.HorizontalAlignment = HorizontalAlignment.Right;
-                        a.Margin = new Thickness(0, margin, 0, 0);
-                        timeGrid.Children.Add(a);
-
-                    }
-                }
-            }
-            //5分钟刻度出现：当间隔小于45分钟
-            if (endsecond - startsecond < 45 * 60)
-            {
-                for (int drawtime = (int)startsecond / 60 / 5; drawtime <= endsecond / 60 / 5; drawtime++)
-                {
-
-                    double margin = timeGrid.ActualHeight * (drawtime * 60 * 5 - startsecond) / (endsecond - startsecond) - 20;
-                    if (margin > -30)
-                    {
-                        TextBlock a = new TextBlock();
-                        a.Text = (drawtime / 12).ToString("D2") + ":" + (drawtime % 12 * 5).ToString("D2");
-                        a.HorizontalAlignment = HorizontalAlignment.Right;
-                        a.Margin = new Thickness(0, margin, 0, 0);
-                        timeGrid.Children.Add(a);
-
-                    }
-
-                }
-            }
-            //1分钟刻度出现：当间隔小于10分钟
-            if (endsecond - startsecond < 10 * 60)
-            {
-                for (int drawtime = (int)startsecond / 60 / 1; drawtime <= endsecond / 60 / 1; drawtime++)
-                {
-
-                    double margin = timeGrid.ActualHeight * (drawtime * 60 - startsecond) / (endsecond - startsecond) - 20;
-                    if (margin > -30)
-                    {
-                        TextBlock a = new TextBlock();
-                        a.Text = (drawtime / 60).ToString("D2") + ":" + (drawtime % 60).ToString("D2");
-                        a.HorizontalAlignment = HorizontalAlignment.Right;
-                        a.Margin = new Thickness(0, margin, 0, 0);
-                        timeGrid.Children.Add(a);
-                    }
-
-
-                }
-            }
         }
 
         private void TimelineGrid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -344,7 +383,7 @@ namespace TimeManagement.Views
             if (pointprop > 0)
             {
                 double totalsecond = (endsecond - startsecond) * 0.2;
-                if (e.Delta > 0)
+                if (e.Delta < 0)
                 {
                     startsecond -= totalsecond * pointprop;
                     endsecond += totalsecond * (1 - pointprop);
