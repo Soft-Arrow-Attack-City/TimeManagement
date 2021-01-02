3c5e617065d8015b79f556692a000daec7b85168
Zhao Qiming <zqm980904@gmail.com>
2021-01-03 02:41:09 +0800

    选用FlaUI解决ui automation测试

---
+++ TimeManagement.Tests/UITest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FlaUI.UIA3;
+using FlaUI.Core;
+
+namespace TimeManagement.Tests
+{
+    public class UITest : IDisposable
+    {
+        static readonly string path = "..\\..\\..\\obj\\Debug\\TimeManagement.exe";
+        private Application app;
+
+        public UITest()
+        {
+            app = Application.Launch(path);
+        }
+
+        public void Dispose()
+        {
+            app.WaitWhileMainHandleIsMissing();
+            app.Close();
+        }
+
+        [Fact]
+        public void LaunchTest()
+        {
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                Assert.NotNull(window);
+                Assert.NotNull(window.Title);
+            }
+        }
+    }
+}
