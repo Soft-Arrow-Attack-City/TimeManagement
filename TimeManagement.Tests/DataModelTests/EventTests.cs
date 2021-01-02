4126affa6bbaa74d6f51c00683db6ffcb4a09bbe
Zhao Qiming <zqm980904@gmail.com>
2021-01-03 00:50:23 +0800

    找回数据结构测试

---
+++ TimeManagement.Tests/DataModelTests/EventTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TimeManagement.DataModel;
+
+namespace TimeManagement.Tests
+{
+    public class EventTests
+    {
+        [Fact]
+        public void TimeTest()
+        {
+            DateTime now = DateTime.Now;
+            TimeEvent timeEvent = new TimeEvent();
+            Assert.True(now <= timeEvent.Created);
+            Assert.True(timeEvent.Created <= DateTime.Now);
+        }
+        [Fact]
+        public void InfoTest()
+        {
+            string title = "Test title";
+            string description = "This is a description";
+            TimeEvent timeEvent = new TimeEvent { Title = title, Description = description };
+            Assert.Contains(title, timeEvent.EventInfo);
+        }
+    }
+}
---
+++ TimeManagement.Tests/DataModelTests/ScheduleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TimeManagement.DataModel;
+using System.Collections;
+
+namespace TimeManagement.Tests
+{
+    public class ScheduleTests
+    {
+        [Theory]
+        [ClassData(typeof(ScheduleTestData))]
+        public void RepeatTest(Freq freq, TimeSpan timeSpan)
+        {
+            MySchedule schedule = new MySchedule { Repeat = freq };
+            MySchedule nextSchedule = schedule.NextSchedule;
+            Assert.Equal(freq, nextSchedule.Repeat);
+            Assert.Equal(timeSpan, nextSchedule.Start - schedule.Start);
+        }
+        [Fact]
+        public void RepeatExceptionTest()
+        {
+            MySchedule schedule = new MySchedule();
+            Assert.Equal(Freq.NoRepeat, schedule.Repeat);
+            Assert.Throws<ArgumentException>(() => schedule.NextSchedule);
+        }
+
+        public class ScheduleTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { Freq.Daily, DateTime.Now.AddDays(1) - DateTime.Now };
+                yield return new object[] { Freq.Weekly, DateTime.Now.AddDays(7) - DateTime.Now };
+                yield return new object[] { Freq.Monthly, DateTime.Now.AddMonths(1) - DateTime.Now };
+                yield return new object[] { Freq.Annual, DateTime.Now.AddYears(1) - DateTime.Now };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
